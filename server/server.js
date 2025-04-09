88f636392227df89806bbac462d4b867b0854f72
Amisha-verma2907 <amuverma543@gmail.com>
2025-04-09 23:22:56 +0530

    route problem solved

--- server/server.js
+++ server/server.js
@@ -13,5 +13,10 @@ app.use(express.json());
 
 app.use('/api/jobs', jobRoutes);
 
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
