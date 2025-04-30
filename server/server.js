54892a8a849188b54d7a5d0dd91fd6651f3fbdb1
Amisha-verma2907 <amuverma543@gmail.com>
2025-04-30 15:20:03 +0530

    new change

--- server/server.js
+++ server/server.js
@@ -8,7 +8,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://student-job-tracker-iota.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/jobs', jobRoutes);
